ec809727ffb0dee0668fd391cb525007b83f0117
PuneetSivananda <puneet.sivananda@gmail.com>
2022-06-25 19:37:55 +0530

    feat: updates

--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -1,2 +1,7 @@
+const path = require("path")
+
 console.log("Hello World")
 console.log(__filename.split("\\").splice(3).join("/"))
+console.log(process.argv)
+
+console.log(path.resolve("index.js"))
