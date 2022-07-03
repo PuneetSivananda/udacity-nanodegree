78f2d95f82ac1b4d412dddbc9ef081d7bcea5d28
PuneetSivananda <puneet.sivananda@gmail.com>
2022-07-04 02:42:06 +0530

    chore: refactor

--- project1/src/index.ts
+++ project1/src/index.ts
@@ -1,9 +1,9 @@
 import express from "express"
 
 const cache = {}
-
 const app = express()
 
-app.listen(process.env.PORT || 3000, () => {
+
+app.listen({host:"localhost", port:3000}, () => {
     console.log(`Started on port 3000`);
 });
