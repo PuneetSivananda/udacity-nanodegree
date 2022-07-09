fad2ffa9131f974d8b6454dae0eb95346ad60f1d
PuneetSivananda <puneet.sivananda@gmail.com>
2022-07-09 16:28:33 +0530

    feat: added typesafety

--- 4_building_node_server/src/utilities/logger.ts
+++ 4_building_node_server/src/utilities/logger.ts
@@ -1,4 +1,6 @@
-const logger = (req:any, res:any, next:any) =>{
+import express from "express"
+
+const logger = (req: express.Request, res: express.Response, next: Function) =>{
     console.log(`${req.baseUrl} was visited`)
     console.log(`Method: ${req.method}, Query: ${req.query}`)
     next()
