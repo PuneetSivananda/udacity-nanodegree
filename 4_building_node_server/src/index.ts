40e995d314dbe8c491e9d8cbb48bbbb902cc6df8
PuneetSivananda <puneet.sivananda@gmail.com>
2022-07-09 16:21:52 +0530

    feat: added routing functionality

--- 4_building_node_server/src/index.ts
+++ 4_building_node_server/src/index.ts
@@ -1,17 +1,16 @@
 import express from "express"
 import logger from "./utilities/logger"
+import routes from "./routes"
+import studentRoutes from "./routes/api/students"
+import teacherRoutes from "./routes/api/teachers"
+
 const app = express()
 const port = 3000
 
 app.use(logger)
-
-app.get("/", (req, res)=>{
-        console.log(
-            "Called api"
-        )
-        res.send("Hello World")
-    }
-)
+app.use("/api", routes)
+app.use("/students", studentRoutes)
+app.use("/teachers", teacherRoutes)
 
 
 app.listen(port, ()=>{
