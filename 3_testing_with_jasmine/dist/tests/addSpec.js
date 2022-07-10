f41f4ea364b356dbba64a752bc256d2e7234b8e1
PuneetSivananda <puneet.sivananda@gmail.com>
2022-07-10 13:17:41 +0530

    feat: added more tests

--- 3_testing_with_jasmine/dist/tests/addSpec.js
+++ 3_testing_with_jasmine/dist/tests/addSpec.js
@@ -5,4 +5,11 @@ describe("Spec for array utilities", () => {
     it("it should add content of array", () => {
         expect((0, arrays_1.addArr)([3, 5])).toBe(8);
     });
+    it("it should concatenate 2 arrays", () => {
+        expect((0, arrays_1.concatArr)([1, 2], [3, 4])).toEqual(jasmine.anything());
+    });
+    it("it should concatenate 2 arrays and return this value", () => {
+        const mixArr = [1, 2, "three", "four"];
+        expect((0, arrays_1.concatArr)([1, 2], ["three", "four"])).toEqual(jasmine.arrayContaining(mixArr));
+    });
 });
--- 3_testing_with_jasmine/src/tests/addSpec.ts
+++ 3_testing_with_jasmine/src/tests/addSpec.ts
@@ -1,7 +1,16 @@
-import { addArr } from "../utilities/arrays"
+import { lgNum, cut3, addArr, concatArr, StringArrOrNumberArr } from "../utilities/arrays"
 
 describe("Spec for array utilities", ()=>{
     it("it should add content of array", ()=>{
         expect(addArr([3, 5])).toBe(8)
+    })
+
+    it("it should concatenate 2 arrays", ()=>{
+        expect(concatArr([1, 2], [3, 4])).toEqual(jasmine.anything())
+    })
+
+    it("it should concatenate 2 arrays and return this value", ()=>{
+        const mixArr = [1, 2, "three", "four"]
+        expect(concatArr([1, 2], ["three", "four"])).toEqual(jasmine.arrayContaining(mixArr))
     })
 })
