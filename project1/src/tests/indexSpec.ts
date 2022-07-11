66a262e824aec1751b3b0910423659a6049d7fed
PuneetSivananda <puneet.sivananda@gmail.com>
2022-07-11 21:40:19 +0530

    feat: setup tests endpoints

--- project1/src/tests/indexSpec.ts
+++ project1/src/tests/indexSpec.ts
@@ -3,10 +3,22 @@ import app from '../index';
 
 const request = supertest(app);
 
-describe('Spec for Testing endpoint responses', () => {
-  it('tests ping endpoint will return 200 status code', async (done) => {
-    const response = await request.get('/ping');
-    expect(response.status).toBe(200);
-    done();
+describe('Spec for Testing Ping endpoints', () => {
+  it('tests ping endpoint will return 200 status code', async () => {
+    const response = await request.get('/api/v1/ping')
+    expect(response.status).toEqual(200)
+  });
+
+  it("tests the response for the ping router", async ()=>{
+    const response = await request.get("/api/v1/ping")
+    expect(response.body.ping).toEqual("pong")
+  })
+});
+
+
+describe('Spec for Testing Image Endpoints', () => {
+  it('tests ping endpoint will return 200 status code', async () => {
+    const response = await request.get('/api/v1/ping')
+    expect(response.status).toEqual(200)
   });
 });
