a8b7fb4c6d8a79675575bc717a5cf11e306c13aa
PuneetSivananda <puneet.sivananda@gmail.com>
2022-07-12 12:38:34 +0530

    feat: added typesafety

--- project1/src/routes/application.ts
+++ project1/src/routes/application.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+import { Request, Response } from 'express';
 import { Router } from 'express';
 import { existsSync } from 'fs';
 import { IRequestItem } from '../models';
@@ -11,53 +12,57 @@ applicationRouter.get('/health', (req, res) => {
   return res.json({ ping: 'pong' });
 });
 
-applicationRouter.get('/resize', urlParser, async (req, res) => {
-  // middle ware to parse url params
-  // from params check if file exists
-  // if exists return in response file
-  // else call file creation
-  // call generate and return the file from generate
+applicationRouter.get(
+  '/resize',
+  urlParser,
+  async (
+    req: Request,
+    res: Response
+  ): Promise<Response<any, Record<string, any>> | void> => {
+    // middle ware to parse url params
+    // from params check if file exists
+    // if exists return in response file
+    // else call file creation
+    // call generate and return the file from generate
 
-  // search in cache
-  const requestObj: IRequestItem = {
-    name: res.locals.name,
-    width: res.locals.width,
-    height: res.locals.height
-  };
-  console.log(requestObj)
-  // find file in path
-  // exists send the file
-  // does not exist generate and send file
+    // search in cache
+    const requestObj: IRequestItem = {
+      name: res.locals.name,
+      width: res.locals.width,
+      height: res.locals.height
+    };
+    console.log(requestObj);
+    // find file in path
+    // exists send the file
+    // does not exist generate and send file
 
-  // handling a custom cache was expensive cause spaghetti code switching to reading from fs
+    // handling a custom cache was expensive cause spaghetti code switching to reading from fs
 
-  const filePath = path.resolve(
-    `./upload/process/${requestObj.name}_${requestObj.width}_${requestObj.height}.jpg`
-  );
-  console.log(filePath)
-  try {
-    if (existsSync(filePath)) {
-      res.statusCode = 200;
-      return res.sendFile(filePath);
-    } else {
-      res.statusCode = 200;
-      const fileDetail = await resizeImage(
-        res.locals.file,
-        parseInt(res.locals.width),
-        parseInt(res.locals.height)
-      );
-      return res.sendFile(`${fileDetail?.destPath}`);
-    }
-  } catch (error) {
-    if (!existsSync(filePath)) {
-      const err = new Error('Unable to open file');
-      res.statusCode = 404;
-      return res.send({
-        message: 'Error Processing Image',
-        errorMessage: err.message
-      });
+    const filePath = path.resolve(
+      `./upload/process/${requestObj.name}_${requestObj.width}_${requestObj.height}.jpg`
+    );
+
+    try {
+      if (existsSync(filePath)) {
+        return res.status(200).sendFile(filePath);
+      } else {
+        const fileDetail = await resizeImage(
+          res.locals.file,
+          parseInt(res.locals.width),
+          parseInt(res.locals.height)
+        );
+        return res.status(200).sendFile(`${fileDetail?.destPath}`);
+      }
+    } catch (error) {
+      if (!existsSync(filePath)) {
+        const err = new Error('Unable to open file');
+        return res.status(404).send({
+          message: 'Error Processing Image',
+          errorMessage: err.message
+        });
+      }
     }
   }
-});
+);
 
 export default applicationRouter;
