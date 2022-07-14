d7372c3b7ac18743ce8f38b22e18e276ea94abe8
PuneetSivananda <puneet.sivananda@gmail.com>
2022-07-14 17:27:23 +0530

    feaT: a functioning todo app

--- project2_courses/redux/index.js
+++ project2_courses/redux/index.js
@@ -1,4 +1,5 @@
-function createStore(){
+//Library Code
+function createStore(reducer){
     let state;
     let listeners = []
     const getState = () => state
@@ -9,21 +10,57 @@ function createStore(){
             listeners = listeners.filter((l)=> l.listerners) 
         }
     }
+
+    const dispatch = (action)=>{
+        state = reducer(state, action)
+        listeners.forEach((listener)=> listener())
+    }
+
     return {
         getState,
-        subscribe
+        subscribe, 
+        dispatch
     }
 }
 
 
-const store = createStore()
+// App Code
+function todos(state=[], action){
+    if (action.type === "ADD_TODO") {
+        return state.concat([action.todo])
+    }
+    return state
+}
 
+// When you create a store pass the specific reducer fuction on the app data
+const store = createStore(todos)
 store.subscribe(()=>{
-    console.log("the new state is ", store.getState())
+    console.log("The new state is ", store.getState())
 })
 
-const unsubscribe = store.subscribe(()=>{
-    console.log("the store changed")
+store.dispatch({
+    type:'ADD_TODO',
+    todo:{
+        id:0,
+        name: "Learn Redux",
+        complete:false
+    }
 })
 
-unsubscribe()
+store.dispatch({
+    type:'ADD_TODO',
+    todo:{
+        id:1,
+        name: "Learn CSS",
+        complete:false
+    }
+})
+
+store.dispatch({
+    type:'ADD_TODO',
+    todo:{
+        id:2,
+        name: "Learn React",
+        complete:false
+    }
+})
