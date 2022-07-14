dd1e1c4ea452719918d43a3347e7a791d16e2c4d
PuneetSivananda <puneet.sivananda@gmail.com>
2022-07-14 17:35:24 +0530

    feat: change the casing to match the todo

--- project2_courses/redux/index.js
+++ project2_courses/redux/index.js
@@ -1,66 +1,71 @@
 //Library Code
-function createStore(reducer){
+function createStore(reducer) {
     let state;
     let listeners = []
     const getState = () => state
 
-    const subscribe = (listener)=>{
+    const subscribe = (listener) => {
         listeners.push(listener)
-        return ()=>{
-            listeners = listeners.filter((l)=> l.listerners) 
+        return () => {
+            listeners = listeners.filter((l) => l.listerners)
         }
     }
 
-    const dispatch = (action)=>{
+    const dispatch = (action) => {
         state = reducer(state, action)
-        listeners.forEach((listener)=> listener())
+        listeners.forEach((listener) => listener())
     }
 
     return {
         getState,
-        subscribe, 
+        subscribe,
         dispatch
     }
 }
 
 
 // App Code
-function todos(state=[], action){
+function todos(state = [], action) {
     if (action.type === "ADD_TODO") {
         return state.concat([action.todo])
+    } else if (action.type === "REMOVE_TODO") {
+        return state.filter(todo => todo.id !== action.id)
+    } else if (action.type === "TOGGLE_TODO") {
+        return state.map((todo) => todo.id !== action.id ? todo : Object.assign({}, todo, { complete: !todo.complete }))
+    } else {
+        return state
     }
-    return state
 }
 
 // When you create a store pass the specific reducer fuction on the app data
 const store = createStore(todos)
-store.subscribe(()=>{
+store.subscribe(() => {
     console.log("The new state is ", store.getState())
 })
 
 store.dispatch({
-    type:'ADD_TODO',
-    todo:{
-        id:0,
+    type: 'ADD_TODO',
+    todo: {
+        id: 0,
         name: "Learn Redux",
-        complete:false
+        complete: false
     }
 })
 
 store.dispatch({
-    type:'ADD_TODO',
-    todo:{
-        id:1,
+    type: 'ADD_TODO',
+    todo: {
+        id: 1,
         name: "Learn CSS",
-        complete:false
+        complete: false
     }
 })
 
 store.dispatch({
-    type:'ADD_TODO',
-    todo:{
-        id:2,
+    type: 'ADD_TODO',
+    todo: {
+        id: 2,
         name: "Learn React",
-        complete:false
+        complete: false
     }
 })
