ec80162f8a4da8c9e94de6893f7517d0c8fd9e1d
PuneetSivananda <puneet.sivananda@gmail.com>
2022-07-26 18:49:40 +0530

    feat: added the file structure

---
+++ project2_courses/react_redux_context/index.js
@@ -0,0 +1,36 @@
+import ReactDOM from "react-dom";
+
+const Parent = ({ name }) => {
+    return (
+        <div>
+            <h1>Parent</h1>
+            <Child name={name} />
+        </div>
+    );
+};
+
+const Child = ({ name }) => {
+    return (
+        <div>
+            <h1>Child</h1>
+            <Grandchild name={name} />
+        </div>
+    );
+};
+
+const Grandchild = ({ name }) => {
+    return (
+        <div>
+            <h1>Grandchild</h1>
+            <h3>Name: {name}</h3>
+        </div>
+    );
+};
+
+const App = () => {
+    const name = "Andrew";
+
+    return <Parent name={name} />;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
