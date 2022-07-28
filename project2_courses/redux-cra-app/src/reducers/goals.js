c0f5e8149f083059269ce65492fdfc3c920b6647
PuneetSivananda <puneet.sivananda@gmail.com>
2022-07-28 10:23:54 +0530

    feat: redux project implemenatation api

---
+++ project2_courses/redux-cra-app/src/reducers/goals.js
@@ -0,0 +1,12 @@
+export function goals(state = [], action) {
+    switch (action.type) {
+        case ADD_GOAL:
+            return state.concat([action.goal])
+        case REMOVE_GOAL:
+            return state.filter((goal) => goal.id !== action.id)
+        case RECIEVE_DATA:
+            return action.goals
+        default:
+            return state
+    }
+}
