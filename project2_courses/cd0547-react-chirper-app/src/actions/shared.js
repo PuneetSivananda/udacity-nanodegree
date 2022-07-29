9872e405c1a351b5dcee74e51aaef3d42d25bb6b
PuneetSivananda <puneet.sivananda@gmail.com>
2022-07-30 00:33:26 +0530

    feat: setup reducers and installed redux

--- project2_courses/cd0547-react-chirper-app/src/actions/shared.js
+++ project2_courses/cd0547-react-chirper-app/src/actions/shared.js
@@ -1,12 +1,16 @@
 import { getInitalData } from "../utils/api"
 import { recieveTweets } from "./tweets"
 import { recieveUsers } from "./users"
+import { setAuthedUser } from "./authUser"
+
+const AUTH_ID = "tylermcginnis"
 
 export function handleInitialData() {
     return (dispatch) => {
         return getInitalData().then(({ users, tweets }) => {
             dispatch(recieveUsers(users))
             dispatch(recieveTweets(tweets))
+            dispatch(setAuthedUser(AUTH_ID))
         })
     }
 }
