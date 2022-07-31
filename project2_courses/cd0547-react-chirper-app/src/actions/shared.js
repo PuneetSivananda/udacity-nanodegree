deb0862328cf37fd68c10211b0ccb89628b9c20e
PuneetSivananda <puneet.sivananda@gmail.com>
2022-07-31 19:41:40 +0530

    feat: added a top loading bar

--- project2_courses/cd0547-react-chirper-app/src/actions/shared.js
+++ project2_courses/cd0547-react-chirper-app/src/actions/shared.js
@@ -2,15 +2,18 @@ import { getInitialData } from "../utils/api"
 import { recieveTweets } from "./tweets"
 import { recieveUsers } from "./users"
 import { setAuthedUser } from "./authUser"
+import { showLoading, hideLoading } from "react-redux-loading-bar"
 
 const AUTH_ID = "tylermcginnis"
 
 export function handleInitialData() {
     return (dispatch) => {
+        dispatch(showLoading())
         return getInitialData().then(({ users, tweets }) => {
             dispatch(recieveUsers(users))
             dispatch(recieveTweets(tweets))
             dispatch(setAuthedUser(AUTH_ID))
+            dispatch(hideLoading())
         })
     }
 }
