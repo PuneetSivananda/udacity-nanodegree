1a558aa0e4340092e7a9c12baef8f4d98a1a2cef
PuneetSivananda <puneet.sivananda@gmail.com>
2022-07-31 21:10:27 +0530

    feat: new tweet component added

---
+++ project2_courses/cd0547-react-chirper-app/src/components/NewTweet.js
@@ -0,0 +1,37 @@
+import { useState } from "react"
+
+const NewTweet = () => {
+    const [text, setText] = useState("")
+    const handleChange = (e) => {
+        const text = e.target.value
+        setText(text)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("New Tweet ", text)
+        setText("")
+    }
+    const tweetLeft = 200 - text.length
+
+    return (
+        <div>
+            <h3 className="center">Compose New Tweet</h3>
+            <form className="new-tweet" onSubmit={handleSubmit}>
+                <textarea
+                    placeholder="What's happening"
+                    value={text}
+                    onChange={handleChange}
+                    className="textarea"
+                    maxLength={280}
+                >
+                </textarea>
+                {tweetLeft <= 100 && <div className="tweet-length">{tweetLeft}</div>}
+                <button className="btn" type="submit" disabled={text === ""}>
+                    Submit
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default NewTweet
